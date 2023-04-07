f11520fa1572c09cbcba4a196220194fa812409b
mmmmyeaaah <mmmmyeaaah@yandex.ru>
2023-04-07 17:50:50 +0400

    connect postgresql

--- store/settings.py
+++ store/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -136,7 +140,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-# Default primary key field type
+# Default primary key field typ
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
